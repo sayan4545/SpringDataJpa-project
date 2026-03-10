009f0c17ee8f71ef4d5cf2a6eadb89d3ce39b636
sayan4545 <sayanchatterjee3010@gmail.com>
2026-03-11 00:58:16 +0530

    Added @patchmapping to update partial user data. Utilised Reflection to invoke the method

--- src/main/java/com/dev/sayan/algocamp/springdatajpaproject/controllers/UserController.java
+++ src/main/java/com/dev/sayan/algocamp/springdatajpaproject/controllers/UserController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -23,6 +24,10 @@ public class UserController {
     @PutMapping(path = "/putEmployee/{id}")
     public ResponseEntity<UserDetails> putEmployee(@RequestBody UserDetails userDetails, @PathVariable Long id){
         return ResponseEntity.ok(userDetailService.putEmployee(id,userDetails));
+    }
+    @PatchMapping(path ="/updatePartialUser/{id}")
+    public ResponseEntity<UserDetails> updatePartialUser(@RequestBody Map<String,Object> updates, @PathVariable Long id){
+        return ResponseEntity.ok(userDetailService.updatePartialUser(id,updates));
     }
 
 

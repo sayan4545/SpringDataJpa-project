0eaefd0f81167e3243c2079750c06cf08676dcba
sayan4545 <sayanchatterjee3010@gmail.com>
2026-03-11 15:44:59 +0530

    Added @DeleteMapping, and g@GetMapping endp[oint to retrieve user by id

--- src/main/java/com/dev/sayan/algocamp/springdatajpaproject/controllers/UserController.java
+++ src/main/java/com/dev/sayan/algocamp/springdatajpaproject/controllers/UserController.java
@@ -1,17 +1,20 @@
 package com.dev.sayan.algocamp.springdatajpaproject.controllers;
 import com.dev.sayan.algocamp.springdatajpaproject.entities.UserDetails;
+import com.dev.sayan.algocamp.springdatajpaproject.repositories.UserDetailsRepository;
 import com.dev.sayan.algocamp.springdatajpaproject.service.UserDetailServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class UserController {
     private final UserDetailServiceImpl userDetailService;
+    private final UserDetailsRepository userDetailsRepository;
 
     @PostMapping(path ="/saveUser")
     public ResponseEntity<UserDetails> saveUser(@RequestBody UserDetails userDetails){
@@ -28,6 +31,15 @@ public class UserController {
     @PatchMapping(path ="/updatePartialUser/{id}")
     public ResponseEntity<UserDetails> updatePartialUser(@RequestBody Map<String,Object> updates, @PathVariable Long id){
         return ResponseEntity.ok(userDetailService.updatePartialUser(id,updates));
+    }
+    @GetMapping(path = "/getById/{id}")
+    public ResponseEntity<Optional<UserDetails>> findUserById(@PathVariable Long id){
+        return ResponseEntity.ok(userDetailService.findUserById(id));
+    }
+
+    @DeleteMapping(path = "/deleteById/{id}")
+    public ResponseEntity<Boolean> deleteUserById(@PathVariable Long id){
+        return ResponseEntity.ok(userDetailService.deleteUserById(id));
     }
 
 

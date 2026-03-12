e51b94ba56c1dddf7165064977a95bb6c19e09f9
sayan4545 <sayanchatterjee3010@gmail.com>
2026-03-13 01:41:31 +0530

    Refactored code so that controller do not return entities directly , instead return dtos and consume dtos. Used modelmapper to map dto to entity and vice versa

--- src/main/java/com/dev/sayan/algocamp/springdatajpaproject/service/UserDetailServiceImpl.java
+++ src/main/java/com/dev/sayan/algocamp/springdatajpaproject/service/UserDetailServiceImpl.java
@@ -1,13 +1,17 @@
 package com.dev.sayan.algocamp.springdatajpaproject.service;
 
+import com.dev.sayan.algocamp.springdatajpaproject.dto.UserDetailsDto;
 import com.dev.sayan.algocamp.springdatajpaproject.entities.UserDetails;
 import com.dev.sayan.algocamp.springdatajpaproject.repositories.UserDetailsRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.util.ReflectionUtils;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -16,23 +20,62 @@ public class UserDetailServiceImpl implements UserService{
     private final UserDetailsRepository userDetailsRepository;
     private final ModelMapper modelMapper;
     @Override
-    public UserDetails saveUser(UserDetails userDetails) {
-        return userDetailsRepository.save(userDetails);
+    public UserDetails saveUser(UserDetailsDto userDetailsDto) {
+        UserDetails user = modelMapper
+                .map(userDetailsDto,UserDetails.class);
+        return userDetailsRepository.save(user);
     }
 
     @Override
-    public List<UserDetails> getAllUsers() {
-        return userDetailsRepository.findAll();
+    public List<UserDetailsDto> getAllUsers() {
+
+        return userDetailsRepository.findAll()
+                .stream()
+                .map(userDetails -> modelMapper.map(userDetails, UserDetailsDto.class))
+                .toList();
     }
 
-    public UserDetails putEmployee(Long id, UserDetails userdetails) {
+    public UserDetailsDto putEmployee(Long id, UserDetailsDto userdetailsDto) {
+        UserDetails userToBeUpdatedTo = modelMapper.map(userdetailsDto,UserDetails.class);
         return userDetailsRepository.findById(id)
                 .map(existingUser -> {
-                    modelMapper.map(userdetails, existingUser);
+                    modelMapper.map(userToBeUpdatedTo, existingUser);
                     existingUser.setId(id);
 
-                    return userDetailsRepository.save(existingUser);
-                })
-                .orElseThrow(() -> new RuntimeException("User not found with id " + id));
+                    userDetailsRepository.save(existingUser);
+                    return modelMapper.map(existingUser,UserDetailsDto.class);
+                }).orElseThrow();
+
+    }
+
+    @Override
+    public UserDetails updatePartialUser(Long id, Map<String, Object> updates) {
+        UserDetails user = userDetailsRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not present with id " + id));
+
+        updates.forEach((fieldName, value) -> {
+            Field field = org.springframework.util.ReflectionUtils.findField(UserDetails.class, fieldName);
+            if (field != null) {
+                field.setAccessible(true);
+                ReflectionUtils.setField(field, user, value);
+            }
+        });
+
+        return userDetailsRepository.save(user);
+    }
+
+    @Override
+    public Optional<UserDetails> findUserById(Long id) {
+        Boolean isExistById = userDetailsRepository.existsById(id);
+        if(!isExistById) throw new RuntimeException("User donot exist ");
+        return userDetailsRepository.findById(id);
+    }
+
+    @Override
+    public Boolean deleteUserById(Long id) {
+        Boolean isExistById = userDetailsRepository.existsById(id);
+        if(!isExistById) return false;
+        userDetailsRepository.deleteById(id);
+        return true;
     }
 }
--- src/main/java/com/dev/sayan/algocamp/springdatajpaproject/service/UserService.java
+++ src/main/java/com/dev/sayan/algocamp/springdatajpaproject/service/UserService.java
@@ -1,12 +1,20 @@
 package com.dev.sayan.algocamp.springdatajpaproject.service;
 
+import com.dev.sayan.algocamp.springdatajpaproject.dto.UserDetailsDto;
 import com.dev.sayan.algocamp.springdatajpaproject.entities.UserDetails;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface UserService {
-    UserDetails saveUser(UserDetails userDetails);
-    List<UserDetails> getAllUsers();
-    UserDetails putEmployee(Long id, UserDetails userdetails);
+    UserDetails saveUser(UserDetailsDto userDetailsDto);
+    List<UserDetailsDto> getAllUsers();
+    UserDetailsDto putEmployee(Long id, UserDetailsDto userdetailsDto);
+
+    UserDetails updatePartialUser(Long id, Map<String,Object> updates);
+
+    Optional<UserDetails> findUserById(Long id);
+
+    Boolean deleteUserById(Long id);
 }

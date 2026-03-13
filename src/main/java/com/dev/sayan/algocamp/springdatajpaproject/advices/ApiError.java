9cc6bcecf0b9937f56e8530352ab51e88b25335f
sayan4545 <sayanchatterjee3010@gmail.com>
2026-03-13 20:09:00 +0530

    Added code to design the api error and exceptons for incorrect input

--- src/main/java/com/dev/sayan/algocamp/springdatajpaproject/advices/ApiError.java
+++ src/main/java/com/dev/sayan/algocamp/springdatajpaproject/advices/ApiError.java
@@ -1,15 +1,19 @@
 package com.dev.sayan.algocamp.springdatajpaproject.advices;
 
 
+import jakarta.validation.constraints.Email;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
+
 @Data
 @Builder
 public class ApiError {
     private HttpStatus httpStatus;
     private String message;
+    private List<String> subErrors;
 }
--- src/main/java/com/dev/sayan/algocamp/springdatajpaproject/advices/GlobalExceptionHandler.java
+++ src/main/java/com/dev/sayan/algocamp/springdatajpaproject/advices/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.dev.sayan.algocamp.springdatajpaproject.advices;
-
 import com.dev.sayan.algocamp.springdatajpaproject.advices.exceptions.ResourceNotFoundException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -25,5 +27,19 @@ public class GlobalExceptionHandler {
                 .message(exception.getMessage())
                 .build();
         return new ResponseEntity<>(error,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleMethodValidationNotAcceptedError(MethodArgumentNotValidException exception){
+        List<String> errors = exception.getBindingResult().getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
+        ApiError error = ApiError.builder()
+                .message("input validation failed")
+                .subErrors(errors)
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .build();
+        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
     }
 }
